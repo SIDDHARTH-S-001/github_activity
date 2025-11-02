e4df5b8a96b324cf176e96eed6f3ddb838dfb889
Siddharth <sid2k01@gmai.com>
2025-11-02 19:30:00 +0000

    update: main - add get_activity method

--- src/main.py
+++ src/main.py
@@ -1,17 +1,19 @@
 import os
 import json
 import requests
+import argparse
 
 class GitHubActivity():
     def __init__(self, filename="default"):
         self.filename = str(filename).lower() + ".json"
+        self.github_url = "https://github.com/"
+        self.base_url = "https://api.github.com/users/"
 
     def get_specific_data(self, username, sort, route=None):
-        base_url = "https://api.github.com/users/"
         if route is None or route=="":
-            url = f"{base_url}{username}"
+            url = f"{self.base_url}{username}"
         else:
-            url = f"{base_url}{username}/{route}"
+            url = f"{self.base_url}{username}/{route}"
         querry_params = {
             "sort": "updated",
             "per_page": sort
@@ -28,7 +30,26 @@ class GitHubActivity():
                 print(self.pat)
         else:
             print(f"The specified path {pat_path} doesn't exist")
-            
+        
+    def get_activity(self, username, n_recent):
+        url = f"{self.base_url}{username}/events"
+        querry_params = {
+            "sort": "updated", 
+            "per_page":n_recent
+        }
+        event = requests.get(url=url, params=querry_params).json()
+        action, createdAt, public = event[0]["type"], event[0]["created_at"], event[0]["public"]
+        repo_name = str(event[0]["repo"]["name"]).split("/")[1]
+        repo_url =  f"{self.github_url}{username}/{repo_name}"
+        return action, createdAt, repo_name, repo_url, public
+    
+    def process_cli(self):
+        parser = argparse.ArgumentParser(description="GitHub activity Tracker")
+        parser.add_argument("username", type=str, help="username of the git user")
+        parser.add_argument("arg1", nargs=1, type=int, help="last 'n' activities of the user")
+        args = parser.parse_args()
+        self.get_activity(args.username, args.arg1)
+
     def to_json(self, data, filename):
         with open(filename, mode="w", encoding="utf-8") as writefile:
             json.dump(data, writefile, indent=2)
@@ -36,10 +57,11 @@ class GitHubActivity():
 if __name__ == "__main__":
     ga = GitHubActivity(filename="user_data")
     username = "SIDDHARTH-S-001"
-    route = "repos"
+    route = "events"
     sort = 1
     pat_path = "/home/kinisi/Documents/personal/pat2.txt"
     # ga.get_specific_data(username=username, sort=sort, route=route)
-    ga.load_credentials(pat_path=pat_path)
+    # ga.load_credentials(pat_path=pat_path)
+    ga.process_cli()
 
 
